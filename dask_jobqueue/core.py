6b35688dd89aba3b0f45d7f9204d29346d31adf0
Joseph Hamman <jhamman@ucar.edu>
2018-05-18 16:39:29 -0600

    pad job id

--- dask_jobqueue/core.py
+++ dask_jobqueue/core.py
@@ -136,7 +136,7 @@ class JobQueueCluster(Cluster):
             self._command_template += " --name %s" % name  # e.g. "dask-worker"
             # Keep %(n) to be replaced later (worker id on this job)
             # ${JOB_ID} is an environment variable describing this job
-            self._command_template += "${JOB_ID}-%(n)d"
+            self._command_template += "-${JOB_ID}-%(n)d"
         if death_timeout is not None:
             self._command_template += " --death-timeout %s" % death_timeout
         if local_directory is not None:
