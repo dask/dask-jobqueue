5a375157c9949d36263fabcb0f8c013bee6021bc
kcotter <kcotter@loni.usc.edu>
2018-09-23 22:40:31 -0700

    remove spurious hyphens

--- ci/sge/run-slave.sh
+++ ci/sge/run-slave.sh
@@ -8,7 +8,7 @@ sleep 4
 sudo service gridengine-exec restart
 
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then
--    python -m SimpleHTTPServer 8888
--else
--    python -m http.server 8888
--fi
+    python -m SimpleHTTPServer 8888
+else
+    python -m http.server 8888
+fi
