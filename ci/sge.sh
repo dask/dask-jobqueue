b7cb4918221c759cca8d1abb95106066d120dc9f
kcotter <kcotter@loni.usc.edu>
2018-10-02 06:13:14 -0700

    Cleanup test merge

--- ci/sge.sh
+++ ci/sge.sh
@@ -3,10 +3,6 @@
 function jobqueue_before_install {
     docker version
     docker-compose version
-    if [ -z "$TRAVIS_PYTHON_VERSION" ]; then
-        export TRAVIS_PYTHON_VERSION='2.7'
-    fi
-    echo "Using Python version: $TRAVIS_PYTHON_VERSION"
     # start sge cluster
     cd ./ci/sge
     ./start-sge.sh
