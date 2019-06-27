1c3edccc08f71c39661b2060ea8caf9fbf10bc1a
Alan O'Cais <a.ocais@fz-juelich.de>
2019-06-27 10:39:30 +0200

    Make sure Python2 tests can pass

--- dask_jobqueue/tests/test_sge.py
+++ dask_jobqueue/tests/test_sge.py
@@ -32,7 +32,12 @@ def test_basic(loop):
             workers = list(client.scheduler_info()["workers"].values())
             w = workers[0]
             assert w["memory_limit"] == 2e9 / 4
-            assert w["nthreads"] == 2
+            if "nthreads" in w:
+                nthreads = "nthreads"
+            else:
+                # for python2
+                nthreads = "ncores"
+            assert w[nthreads] == 2
 
             cluster.scale(0)
 
