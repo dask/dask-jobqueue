ff89fadc68946321d79d1b72c6f518d9adfbd910
Loïc Estève <loic.esteve@ymail.com>
2020-05-01 15:09:05 +0200

    iter

--- ci/slurm/start-slurm.sh
+++ ci/slurm/start-slurm.sh
@@ -8,7 +8,7 @@ while [ `echo $bla | grep "sacctmgr: error" | wc -l` -ne 0 ]
     echo Cluster is not ready
     echo "$bla"
     docker-compose ps
-    docker-compose ps | grep slurmdbd && docker-compose start slurmdbd || echo slurmdbd alive
+    # docker-compose ps | grep slurmdbd && docker-compose start slurmdbd || echo slurmdbd alive
     echo "Waiting for SLURM cluster to become ready";
     sleep 2
     bla=$(./register_cluster.sh 2>&1)
