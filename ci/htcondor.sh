49c8808c2bb8ad953782d3966afeda84b6bda7a0
Till Riedel <riedel@teco.edu>
2020-06-12 02:35:25 +0200

    disable tty-allocation in CI

--- ci/htcondor.sh
+++ ci/htcondor.sh
@@ -16,18 +16,18 @@ function jobqueue_before_install {
 
 function jobqueue_install {
     cd ./ci/htcondor
-    docker-compose exec submit /bin/bash -c "cd /dask-jobqueue; pip3 install -e .;chown -R submituser ."
+    docker-compose exec -T submit /bin/bash -c "cd /dask-jobqueue; pip3 install -e .;chown -R submituser ."
     cd -
 }
 
 function jobqueue_script {
     cd ./ci/htcondor
-    docker-compose exec --user submituser submit /bin/bash -c "cd; pytest /dask-jobqueue/dask_jobqueue --verbose -E htcondor -s"
+    docker-compose exec -T --user submituser submit /bin/bash -c "cd; pytest /dask-jobqueue/dask_jobqueue --verbose -E htcondor -s"
     cd -
 }
 
 function jobqueue_after_script {
     cd ./ci/htcondor
-    docker-compose exec cm /bin/bash -c " grep -R \"\" /var/log/condor/	"
+    docker-compose exec -T cm /bin/bash -c " grep -R \"\" /var/log/condor/	"
     cd -
 }
--- ci/htcondor/start-htcondor.sh
+++ ci/htcondor/start-htcondor.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d
 
-while [ `docker-compose exec submit condor_status -af activity|grep Idle|wc -l` -ne 2 ]
+while [ `docker-compose exec -T submit condor_status -af activity|grep Idle|wc -l` -ne 2 ]
   do
     echo "Waiting for cluster to become ready";
     sleep 2
