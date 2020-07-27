59f151a76d20e2018477ea43c1b455a9abc07bd5
Loïc Estève <loic.esteve@ymail.com>
2020-07-27 16:52:06 +0200

    config_name can be set at the job level.
    and scheduler_name is an old attribute that is not used anymore.

--- dask_jobqueue/moab.py
+++ dask_jobqueue/moab.py
@@ -4,10 +4,9 @@ from .pbs import PBSJob, PBSCluster
 class MoabJob(PBSJob):
     submit_command = "msub"
     cancel_command = "canceljob"
-    scheduler_name = "moab"
+    config_name = "moab"
 
 
 class MoabCluster(PBSCluster):
     __doc__ = PBSCluster.__doc__.replace("PBSCluster", "MoabCluster")
     job_cls = MoabJob
-    config_name = "moab"
