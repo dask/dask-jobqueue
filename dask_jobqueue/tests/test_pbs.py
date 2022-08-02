77fe2713e8f6232f0b80701d42d9e56bf3aa48d9
Guillaume EB <g.eynard.bontemps@gmail.com>
2022-08-02 23:27:42 +0400

    Fix PBS Issue

--- dask_jobqueue/tests/test_pbs.py
+++ dask_jobqueue/tests/test_pbs.py
@@ -160,7 +160,7 @@ def test_scale_cores_memory(loop):
 
             workers = list(client.scheduler_info()["workers"].values())
             w = workers[0]
-            assert w["memory_limit"] == 2**1024**3
+            assert w["memory_limit"] == 2 * 1024**3
             assert w["nthreads"] == 2
 
             cluster.scale(memory="0GB")
