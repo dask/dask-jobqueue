71b17b1680edc1e3979e3f41d2900db99bc578e4
Johannes Lange <johannes.lange@uni-hamburg.de>
2022-08-08 17:12:15 +0200

    adapt assertion to 500MiB

--- dask_jobqueue/tests/test_htcondor.py
+++ dask_jobqueue/tests/test_htcondor.py
@@ -92,7 +92,7 @@ def test_basic(loop):
 
                 workers = list(client.scheduler_info()["workers"].values())
                 w = workers[0]
-                assert w["memory_limit"] == 100 * 1024**2
+                assert w["memory_limit"] == 500 * 1024**2
                 assert w["nthreads"] == 1
 
                 cluster.scale(0)
