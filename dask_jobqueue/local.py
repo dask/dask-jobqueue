d18068fd5ccc28c95fcdaa47e8eaf31184dab82c
Johannes Lange <johannes.lange@uni-hamburg.de>
2022-08-19 10:28:26 +0200

    local

--- dask_jobqueue/local.py
+++ dask_jobqueue/local.py
@@ -32,7 +32,7 @@ class LocalJob(Job):
         project=None,
         resource_spec=None,
         walltime=None,
-        job_extra=None,
+        job_extra_directives=None,
         config_name=None,
         **kwargs
     ):
